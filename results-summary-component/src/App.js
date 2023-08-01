40096fb07b259687b69707aa7cf98a77e60c68d2
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-02 03:10:06 +0530

    now we able to access the data from JSON file

--- results-summary-component/src/App.js
+++ results-summary-component/src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";  
+import data from "./data.json";
+
 export default function App(){
+  const [items] = useState(data);
+
   return (
-    <h1>Hello world !!!</h1>
-  )
+    <section>
+      <div>
+        <h2>Your Result</h2>
+        <p><span>76</span>of 100</p>
+
+        <h2>Great</h2>
+        <p>You scored higher than 65% of the people who have taken these tests.</p>
+      </div>
+
+      <div>
+        {items.length} items
+      </div>
+    </section>
+  );
 }
