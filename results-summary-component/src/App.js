49cc552eed1c4822d53ba535a5f2e726c9038345
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-02 03:59:18 +0530

    we added new designs to text
    here we are added to div separately result div get the rounded corner and summary div get the text edit margin and padding

--- results-summary-component/src/App.js
+++ results-summary-component/src/App.js
@@ -6,36 +6,44 @@ export default function App(){
 
   return (
     <section>
-      <div>
-        <h2>Your Result</h2>
-        <p><span>76</span>of 100</p>
+      <div className="bg-indigo-500 p-8 text-center text-white rounded-b-3xl">
+        <p className="mb-6">Your Result</p>
+        <div className="flex items-center justify-center">
+          <p className="bg-indigo-600 w-24 h-24 flex flex-col items-center 
+                        justify-center gap-1 rounded-full 
+                        text-white text-3xl font-bold">
+            76<p className="text-slate-200 text-base font-normal">of 100</p>
+          </p>
+        </div>
 
-        <h2>Great</h2>
+        <h2 className="my-6">Great</h2>
         <p>You scored higher than 65% of the people who have taken these tests.</p>
       </div>
 
-      <div>
-        <h2>Summary</h2>
+      <div className="p-8 ">
+        <h2 className="text-slate-700 mb-6 text-lg font-bold">Summary</h2>
 
-        <div>
+        <div className="flex flex-col gap-4">
             {items.map((item, index) => (
               <div
                 key={index}
                 style={{
-                  backgroundColor: item.color,
+                  backgroundColor: `${item.color}15`,
                 }}
+                className="flex items-center justify-between p-4 rounded-lg"
               >
                 <h3
                   style={{
                     color: item.color,
                   }}
+                  className="flex items-center gap-2"
                 >
                   <img src={item.icon} alt={item.category} />
                   {item.category}
                 </h3>
 
-                <p>
-                  <span>{item.score}</span>{" "}
+                <p className="flex items-center gap-2 text-slate-500">
+                  <span className="text-slate-700 font-bold">{item.score}</span>{" "}
                   / 100
                 </p>
               </div>
@@ -47,68 +55,3 @@ export default function App(){
     </section>
   );
 }
-
-// import { useState } from "react";
-// import data from "./data.json";
-
-// export default function App() {
-//   const [items] = useState(data);
-
-//   return (
-//     <div className="md:flex md:items-center md:justify-center md:h-screen md:max-w-2xl md:mx-auto">
-//       <section className="md:flex bg-white shadow rounded-3xl">
-//         <div className="bg-div p-8 md:pt-10 text-center text-white rounded-b-3xl md:flex-1 md:rounded-3xl md:flex md:flex-col md:items-center md:justify-center">
-//           <p className="mb-6">Your Result</p>
-
-//           <div className="flex items-center justify-center">
-//             <p className="bg-circle w-24 h-24 md:w-40 md:h-40 flex flex-col items-center justify-center gap-1 rounded-full text-white text-3xl md:text-6xl font-bold">
-//               76 <p className="text-slate-200 text-base font-normal">of 100</p>
-//             </p>
-//           </div>
-
-//           <h2 className="my-6 text-white font-bold text-lg md:text-2xl">
-//             Great
-//           </h2>
-//           <p>
-//             You scored higher than 65% of the people who have taken these tests.
-//           </p>
-//         </div>
-
-//         <div className="p-8 md:flex-1">
-//           <h2 className="text-slate-700 mb-6 text-lg font-bold">Summary</h2>
-
-//           <div className="flex flex-col gap-4">
-//             {items.map((item, index) => (
-//               <div
-//                 key={index}
-//                 style={{
-//                   backgroundColor: `${item.color}10`,
-//                 }}
-//                 className="flex items-center justify-between p-4 rounded-lg"
-//               >
-//                 <h3
-//                   style={{
-//                     color: item.color,
-//                   }}
-//                   className="flex items-center gap-2"
-//                 >
-//                   <img src={item.icon} alt={item.category} />
-//                   {item.category}
-//                 </h3>
-
-//                 <p className="flex items-center gap-2 text-slate-500">
-//                   <span className="text-slate-700 font-bold">{item.score}</span>{" "}
-//                   / 100
-//                 </p>
-//               </div>
-//             ))}
-//           </div>
-
-//           <button className="bg-slate-700 py-4 px-6 rounded-full text-white w-full mt-6 hover:bg-indigo-600 transition-all duration-200">
-//             Continue
-//           </button>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
